6c84befe66d71bd3587b7de751f406d35e945842
Tautvydas Juškys <taujus1@ktu.lt>
2022-09-23 17:58:11 +0300

    implement endpoints for player

---
+++ BattleshipsApi/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BattleshipsApi.DTO;
+using BattleshipsApi.Models;
+
+namespace BattleshipsApi
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Player, AddPlayerDto>();
+            CreateMap<AddPlayerDto, Player>();
+        }
+    }
+}
