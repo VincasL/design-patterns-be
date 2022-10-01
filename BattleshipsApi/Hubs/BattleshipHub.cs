4663a02dcc6cf7f1d46ca1cdf23d3ba0706de8de
Tautvydas Juškys <taujus1@ktu.lt>
2022-10-01 13:48:21 +0300

    signalR changes

--- BattleshipsApi/Hubs/BattleshipHub.cs
+++ BattleshipsApi/Hubs/BattleshipHub.cs
@@ -6,5 +6,20 @@ namespace BattleshipsApi.Hubs;
 [SignalRHub]
 public class BattleshipHub : Hub
 {
+    public async Task askServer(string someTextFromClient)
+    {
+        string tempString;
+
+        if(someTextFromClient == "hey")
+        {
+            tempString = "message was 'hey'";
+        }
+        else
+        {
+            tempString = "message was something else";
+        }
+
+        await Clients.Clients(this.Context.ConnectionId).SendAsync("askServerResponse", tempString);
+    }
 
 }
