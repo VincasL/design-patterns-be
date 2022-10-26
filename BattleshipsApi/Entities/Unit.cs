ef265a6a58c899c61e4986036d0f02c5d52802ba
Tautvydas Steponavičius <t.steponavicius@icloud.com>
2022-10-26 20:27:10 +0300

    Implemented prototype design pattern on Unit class

--- BattleshipsApi/Entities/Unit.cs
+++ BattleshipsApi/Entities/Unit.cs
@@ -2,9 +2,14 @@
 
 namespace BattleshipsApi.Entities
 {
-    public class Unit
+    public class Unit: ICloneable
     {
         public int Length { get; set; }
         public MoveStrategy MoveStrategy=new DontMove();
+
+        public object Clone()
+        {
+            return (Unit)MemberwiseClone();
+        }
     }
 }
