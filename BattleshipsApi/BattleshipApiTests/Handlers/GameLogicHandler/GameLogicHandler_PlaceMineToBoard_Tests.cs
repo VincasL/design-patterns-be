a970942fe77ce4d563236a53ca2581a14cd17383
Tautvydas Juškys <taujus1@ktu.lt>
2022-11-24 23:07:01 +0200

    Write tests for place mine to board and place ship to board

--- BattleshipsApi/BattleshipApiTests/Handlers/GameLogicHandler/GameLogicHandler_PlaceMineToBoard_Tests.cs
+++ BattleshipsApi/BattleshipApiTests/Handlers/GameLogicHandler/GameLogicHandler_PlaceMineToBoard_Tests.cs
@@ -1,3 +1,5 @@
+using BattleshipsApi.Entities.Mines;
+
 namespace BattleshipApiTests.Handlers.GameLogicHandler;
 
 public class GameLogicHandler_PlaceMineToBoard_Tests
@@ -7,7 +9,86 @@ public class GameLogicHandler_PlaceMineToBoard_Tests
     [SetUp]
     public void Setup()
     {
-        //TODO: tests
+
     }
-    
+
+    [Test]
+    public void PlaceSmallMineToBoard()
+    {
+        //Arrange
+        var coordinates = new CellCoordinates(0, 0);
+        var board = new Board(1);
+        var mine = new SmallMine();
+
+        //Act & Assert
+        _gameLogicHandler.Invoking(handler => handler.PlaceMineToBoard(mine, board, coordinates))
+            .Should().NotThrow();
+    }
+
+    [Test]
+    public void PlaceRemoteMineToBoard()
+    {
+        //Arrange
+        var coordinates = new CellCoordinates(0, 0);
+        var board = new Board(1);
+        var mine = new RemoteControlledMIne();
+
+        //Act & Assert
+        _gameLogicHandler.Invoking(handler => handler.PlaceMineToBoard(mine, board, coordinates))
+            .Should().NotThrow();
+    }
+
+    [Test]
+    public void PlaceHugeMineToBoard_overflow()
+    {
+        //Arrange
+        var coordinates = new CellCoordinates(0, 0);
+        var board = new Board(1);
+        var mine = new HugeMine();
+
+        //Act & Assert
+        _gameLogicHandler.Invoking(handler => handler.PlaceMineToBoard(mine, board, coordinates))
+            .Should().Throw<Exception>();
+    }
+
+    [Test]
+    public void PlaceHugeMineToBoard()
+    {
+        //Arrange
+        var coordinates = new CellCoordinates(1, 1);
+        var board = new Board(10);
+        var mine = new HugeMine();
+
+        //Act & Assert
+        _gameLogicHandler.Invoking(handler => handler.PlaceMineToBoard(mine, board, coordinates))
+            .Should().NotThrow();
+    }
+
+    [Test]
+    public void PlaceNullMineToBoard()
+    {
+        //Arrange
+        var coordinates = new CellCoordinates(1, 1);
+        var board = new Board(10);
+        Mine mine = null;
+
+        //Act & Assert
+        _gameLogicHandler.Invoking(handler => handler.PlaceMineToBoard(mine, board, coordinates))
+            .Should().Throw<Exception>();
+    }
+
+    [Test]
+    public void PlaceMineOnMineToBoard()
+    {
+        //Arrange
+        var coordinates = new CellCoordinates(1, 1);
+        var board = new Board(10);
+        var mine = new HugeMine();
+
+        //Act & Assert
+        _gameLogicHandler.PlaceMineToBoard(mine, board, coordinates);
+        _gameLogicHandler.Invoking(handler => handler.PlaceMineToBoard(mine, board, coordinates))
+            .Should().Throw<Exception>();
+    }
+
 }
--- BattleshipsApi/BattleshipApiTests/Handlers/GameLogicHandler/GameLogicHandler_PlaceShipToBoard_Tests.cs
+++ BattleshipsApi/BattleshipApiTests/Handlers/GameLogicHandler/GameLogicHandler_PlaceShipToBoard_Tests.cs
@@ -1,3 +1,5 @@
+using BattleshipsApi.Entities.Mines;
+
 namespace BattleshipApiTests.Handlers.GameLogicHandler;
 
 public class GameLogicHandler_PlaceShipToBoard_Tests
@@ -7,7 +9,117 @@ public class GameLogicHandler_PlaceShipToBoard_Tests
     [SetUp]
     public void Setup()
     {
-        //TODO: tests
     }
-    
+
+    [Test]
+    public void PlaceBattleshipToBoard()
+    {
+        //Arrange
+        var coordinates = new CellCoordinates(0, 0);
+        var board = new Board(10);
+        var battleship = new Battleship();
+
+        //Act & Assert
+        _gameLogicHandler.Invoking(handler => handler.PlaceShipToBoard(battleship, board, coordinates))
+                    .Should().NotThrow();
+    }
+
+    [Test]
+    public void PlaceCarrierToBoard()
+    {
+        //Arrange
+        var coordinates = new CellCoordinates(0, 0);
+        var board = new Board(10);
+        var carrier = new Carrier();
+
+        //Act & Assert
+        _gameLogicHandler.Invoking(handler => handler.PlaceShipToBoard(carrier, board, coordinates))
+                .Should().NotThrow();
+    }
+
+    [Test]
+    public void PlaceCruiserToBoard()
+    {
+        //Arrange
+        var coordinates = new CellCoordinates(0, 0);
+        var board = new Board(10);
+        var cruiser = new Cruiser();
+
+        //Act & Assert
+        _gameLogicHandler.Invoking(handler => handler.PlaceShipToBoard(cruiser, board, coordinates))
+                    .Should().NotThrow();
+    }
+
+    [Test]
+    public void PlaceDestroyerToBoard()
+    {
+        //Arrange
+        var coordinates = new CellCoordinates(0, 0);
+        var board = new Board(10);
+        var destroyer = new Destroyer();
+
+        //Act & Assert
+        _gameLogicHandler.Invoking(handler => handler.PlaceShipToBoard(destroyer, board, coordinates))
+                    .Should().NotThrow();
+    }
+
+    [Test]
+    public void PlaceSubmarineToBoard()
+    {
+        //Arrange
+        var coordinates = new CellCoordinates(0, 0);
+        var board = new Board(10);
+        var submarine = new Submarine();
+
+        //Act & Assert
+        _gameLogicHandler.Invoking(handler => handler.PlaceShipToBoard(submarine, board, coordinates))
+                    .Should().NotThrow();
+    }
+
+    [Test]
+    public void PlaceBattleshipOverlapToBoard()
+    {
+        //Arrange
+        var coordinates = new CellCoordinates(0, 0);
+        var board = new Board(10);
+        var battleship = new Battleship();
+
+        _gameLogicHandler.PlaceShipToBoard(battleship, board, coordinates);
+
+        //Act & Assert
+        _gameLogicHandler.Invoking(handler => handler.PlaceShipToBoard(battleship, board, coordinates))
+                    .Should().Throw<Exception>();
+    }
+
+    [Test]
+    public void PlaceBattleshipHorizontalToBoard()
+    {
+        //Arrange
+        var coordinates = new CellCoordinates(0, 0);
+        var board = new Board(10);
+        var battleship = new Battleship();
+        battleship.IsHorizontal = true;
+
+        //Act & Assert
+        _gameLogicHandler.Invoking(handler => handler.PlaceShipToBoard(battleship, board, coordinates))
+                    .Should().NotThrow();
+    }
+
+
+    [Test]
+    public void PlaceBattleshipHorizontalOverlapToBoard()
+    {
+        //Arrange
+        var coordinates = new CellCoordinates(0, 0);
+        var board = new Board(10);
+        var battleship = new Battleship();
+        battleship.IsHorizontal = true;
+
+        _gameLogicHandler.PlaceShipToBoard(battleship, board, coordinates);
+
+        //Act & Assert
+        _gameLogicHandler.Invoking(handler => handler.PlaceShipToBoard(battleship, board, coordinates))
+                    .Should().Throw<Exception>();
+    }
+
 }
